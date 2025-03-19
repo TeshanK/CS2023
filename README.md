49706fa1588edd7780adb16097511dd18b0210d4
TeshanK <90088670+TeshanK@users.noreply.github.com>
2025-03-19 13:38:39 +0530

    completed bubble sort

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## Codes for CS2023 - Data Structures and Algorithms module
 
 - Insertion Sort ✅
+- Bubble Sort ✅
--- bubblesort.cc
+++ bubblesort.cc
@@ -1,1 +1,13 @@
 #include "driver.h"
+
+/* Function to sort an array using Bubble Sort */
+void bubbleSort(vector<int>& arr)
+{
+    int n = arr.size();
+    for(int i = 0; i < n; i++){
+        for(int j = 1; j < n; j++){
+            if(arr[j - 1] > arr[j])
+                swap(arr[j - 1], arr[j]);
+        }
+    }
+}
