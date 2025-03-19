7bfe480964cebb17f1248456b877a0c4d85224be
TeshanK <90088670+TeshanK@users.noreply.github.com>
2025-03-19 13:40:18 +0530

    ..

--- bubblesort.cc
+++ bubblesort.cc
@@ -4,10 +4,14 @@
 void bubbleSort(vector<int>& arr)
 {
     int n = arr.size();
+
     for(int i = 0; i < n; i++){
+
         for(int j = 1; j < n; j++){
+
             if(arr[j - 1] > arr[j])
                 swap(arr[j - 1], arr[j]);
+        
         }
     }
 }
